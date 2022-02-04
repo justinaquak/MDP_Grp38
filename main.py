865d53011268c41149350fad1e579466cc00cbbb
PickleWeed <62238205+PickleWeed@users.noreply.github.com>
2022-02-04 17:20:50 +0800

    update STM

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#from stm import *
+from stm import *
 #from android import *
 from pc import *
 #from config import *
@@ -39,6 +39,19 @@ class RaspberryPi(threading.Thread):
         # STM read and write thread
         readSTMThread = threading.Thread(target = self.read_STM, args = (), name = "read_STM_thread")
         writeSTMThread = threading.Thread(target = self.write_STM, args = (), name = "write_STM_thread")
+
+        # PC read and write thread
+        readPCthread = threading.Thread(target = self.read_PC, args = (), name = "read_pc_thread")
+        writePCthread = threading.Thread(target = self.write_PC, args = (), name = "write_pc_thread")
+
+        # start running the thread for PC
+        readPCthread.start()
+
+        # Start running thread for Android
+        readAndroidThread.start()
+
+        # Start running thread for STM
+        readSTMThread.start()
 
 
 
